1336f6ad658c83da06fe4bf2bf588456924eeefb
Damian Senn <damian.senn@topaxi.codes>
2024-01-17 22:32:15 +0100

    wip: something like this?

--- crates/tlang_ast/src/visit.rs
+++ crates/tlang_ast/src/visit.rs
@@ -1,6 +1,113 @@
 use crate::node;
 
 pub trait Visitor<'ast>: Sized {
-    // TODO: Create structs for each node type.
-    fn visit_module(&mut self, _program: &'ast node::Node) {}
+    fn visit_module(&mut self, statements: &'ast [node::Node]) {
+        walk_module(self, statements);
+    }
+
+    fn visit_block(
+        &mut self,
+        statements: &'ast [node::Node],
+        expression: &'ast Option<node::Node>,
+    ) {
+        walk_block(self, statements, expression);
+    }
+
+    fn visit_statement(&mut self, statement: &'ast node::Node) {
+        walk_statement(self, statement);
+    }
+
+    fn visit_function_declaration(&mut self, declaration: &'ast node::FunctionDeclaration) {
+        walk_function_declaration(self, declaration);
+    }
+
+    fn visit_expression(&mut self, expression: &'ast node::Node) {
+        walk_expression(self, expression);
+    }
+
+    fn visit_function_parameter(&mut self, _parameter: &'ast node::Node) {
+        todo!()
+    }
+
+    fn visit_function_guard(&mut self, _guard: &'ast node::Node) {
+        todo!()
+    }
+
+    fn visit_function_return_type_annotation(&mut self, _annotation: &'ast node::Node) {
+        todo!()
+    }
+
+    fn visit_function_body(&mut self, body: &'ast node::Node) {
+        walk_function_body(self, body);
+    }
+}
+
+pub fn walk_module<'ast, V: Visitor<'ast>>(visitor: &mut V, statements: &'ast [node::Node]) {
+    for statement in statements {
+        visitor.visit_statement(statement);
+    }
+}
+
+pub fn walk_block<'ast, V: Visitor<'ast>>(
+    visitor: &mut V,
+    statements: &'ast [node::Node],
+    expression: &'ast Option<node::Node>,
+) {
+    for statement in statements {
+        visitor.visit_statement(statement);
+    }
+    if let Some(expression) = expression {
+        visitor.visit_expression(expression);
+    }
+}
+
+pub fn walk_statement<'ast, V: Visitor<'ast>>(visitor: &mut V, statement: &'ast node::Node) {
+    match &statement.ast_node {
+        node::AstNode::None => {}
+        node::AstNode::Block(statements, expression) => {
+            visitor.visit_block(statements, expression);
+        }
+        node::AstNode::FunctionDeclaration(declaration) => {
+            visitor.visit_function_declaration(declaration);
+        }
+        _ => todo!(),
+    }
+}
+
+pub fn walk_function_body<'ast, V: Visitor<'ast>>(visitor: &mut V, body: &'ast node::Node) {
+    visitor.visit_expression(body);
+}
+
+pub fn walk_function_declaration<'ast, V: Visitor<'ast>>(
+    visitor: &mut V,
+    declaration: &'ast node::FunctionDeclaration,
+) {
+    for parameter in &declaration.parameters {
+        visitor.visit_function_parameter(parameter)
+    }
+    if let Some(ref guard) = *declaration.guard {
+        visitor.visit_function_guard(guard);
+    }
+    if let Some(ref return_type_annotation) = *declaration.return_type_annotation {
+        visitor.visit_function_return_type_annotation(return_type_annotation);
+    }
+    visitor.visit_function_body(&declaration.body);
+}
+
+pub fn walk_expression<'ast, V: Visitor<'ast>>(visitor: &mut V, expression: &'ast node::Node) {
+    visitor.visit_expression(expression);
+    match &expression.ast_node {
+        node::AstNode::None => {}
+        node::AstNode::Block(statements, expression) => {
+            visitor.visit_block(statements, expression);
+        }
+        node::AstNode::BinaryOp { op: _, lhs, rhs } => {
+            visitor.visit_expression(lhs);
+            visitor.visit_expression(rhs);
+        }
+        node::AstNode::UnaryOp(_, expression) => {
+            visitor.visit_expression(expression);
+        }
+        _ => todo!(),
+    }
 }
