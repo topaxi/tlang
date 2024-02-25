e0c7175cf064816d2a9aa11f3e206f0eb1c341c8
Damian Senn <damian.senn@topaxi.codes>
2024-02-25 16:49:33 +0100

    feat: visitor

--- crates/tlang_ast/src/visit.rs
+++ crates/tlang_ast/src/visit.rs
@@ -13,39 +13,79 @@ pub trait Visitor<'ast>: Sized {
         walk_block(self, statements, expression);
     }
 
-    fn visit_statement(&mut self, statement: &'ast node::Stmt) {
-        walk_statement(self, statement);
+    fn visit_ident(&mut self, _ident: &'ast node::Ident) {}
+
+    fn visit_pat(&mut self, pattern: &'ast node::Pattern) {
+        walk_pat(self, pattern);
     }
 
-    fn visit_function_declaration(&mut self, declaration: &'ast node::FunctionDeclaration) {
-        walk_function_declaration(self, declaration);
+    fn visit_stmt(&mut self, statement: &'ast node::Stmt) {
+        walk_stmt(self, statement);
     }
 
-    fn visit_expression(&mut self, expression: &'ast node::Expr) {
-        walk_expression(self, expression);
+    fn visit_fn_decl(&mut self, declaration: &'ast node::FunctionDeclaration) {
+        walk_fn_decl(self, declaration);
     }
 
-    fn visit_function_parameter(&mut self, _parameter: &'ast node::FunctionParameter) {
-        todo!()
+    fn visit_expr(&mut self, expression: &'ast node::Expr) {
+        walk_expr(self, expression);
     }
 
-    fn visit_function_guard(&mut self, _guard: &'ast node::Expr) {
-        todo!()
+    fn visit_fn_param(&mut self, parameter: &'ast node::FunctionParameter) {
+        walk_pat(self, &parameter.pattern);
+
+        if let Some(ref type_annotation) = *parameter.type_annotation {
+            walk_ty(self, &type_annotation);
+        }
     }
 
-    fn visit_function_return_type_annotation(&mut self, _annotation: &'ast node::Ty) {
-        todo!()
+    fn visit_fn_guard(&mut self, guard: &'ast node::Expr) {
+        walk_expr(self, guard);
     }
 
-    fn visit_function_body(&mut self, body: &'ast node::Expr) {
-        walk_function_body(self, body);
+    fn visit_fn_ret_ty(&mut self, annotation: &'ast node::Ty) {
+        walk_ty(self, annotation);
+    }
+
+    fn visit_fn_body(&mut self, body: &'ast node::Expr) {
+        walk_fn_body(self, body);
     }
 }
 
 pub fn walk_module<'ast, V: Visitor<'ast>>(visitor: &mut V, module: &'ast node::Module) {
     for statement in &module.statements {
-        visitor.visit_statement(statement);
+        visitor.visit_stmt(statement);
     }
+}
+
+pub fn walk_pat<'ast, V: Visitor<'ast>>(visitor: &mut V, pattern: &'ast node::Pattern) {
+    match &pattern.kind {
+        node::PatternKind::Identifier { name, .. } => visitor.visit_ident(name),
+        node::PatternKind::Rest(pattern) => visitor.visit_pat(pattern),
+        node::PatternKind::List(patterns) => {
+            for pattern in patterns {
+                visitor.visit_pat(pattern);
+            }
+        }
+        node::PatternKind::Enum {
+            identifier,
+            elements,
+            ..
+        } => {
+            visitor.visit_expr(identifier);
+            for element in elements {
+                visitor.visit_pat(element);
+            }
+        }
+        node::PatternKind::Literal(literal) => {
+            visitor.visit_expr(literal);
+        }
+        node::PatternKind::Wildcard => {}
+    }
+}
+
+pub fn walk_ty<'ast, V: Visitor<'ast>>(_visitor: &mut V, _ty: &'ast node::Ty) {
+    todo!()
 }
 
 pub fn walk_block<'ast, V: Visitor<'ast>>(
@@ -54,58 +94,58 @@ pub fn walk_block<'ast, V: Visitor<'ast>>(
     expression: &'ast Option<node::Expr>,
 ) {
     for statement in statements {
-        visitor.visit_statement(statement);
+        visitor.visit_stmt(statement);
     }
     if let Some(expression) = expression {
-        visitor.visit_expression(expression);
+        visitor.visit_expr(expression);
     }
 }
 
-pub fn walk_statement<'ast, V: Visitor<'ast>>(visitor: &mut V, statement: &'ast node::Stmt) {
+pub fn walk_stmt<'ast, V: Visitor<'ast>>(visitor: &mut V, statement: &'ast node::Stmt) {
     match &statement.kind {
         node::StmtKind::None => {}
         node::StmtKind::Expr(expr) => {
-            visitor.visit_expression(expr);
+            visitor.visit_expr(expr);
         }
         node::StmtKind::FunctionDeclaration(declaration) => {
-            visitor.visit_function_declaration(declaration);
+            visitor.visit_fn_decl(declaration);
         }
         _ => todo!(),
     }
 }
 
-pub fn walk_function_body<'ast, V: Visitor<'ast>>(visitor: &mut V, body: &'ast node::Expr) {
-    visitor.visit_expression(body);
+pub fn walk_fn_body<'ast, V: Visitor<'ast>>(visitor: &mut V, body: &'ast node::Expr) {
+    visitor.visit_expr(body);
 }
 
-pub fn walk_function_declaration<'ast, V: Visitor<'ast>>(
+pub fn walk_fn_decl<'ast, V: Visitor<'ast>>(
     visitor: &mut V,
     declaration: &'ast node::FunctionDeclaration,
 ) {
     for parameter in &declaration.parameters {
-        visitor.visit_function_parameter(parameter)
+        visitor.visit_fn_param(parameter)
     }
     if let Some(ref guard) = *declaration.guard {
-        visitor.visit_function_guard(guard);
+        visitor.visit_fn_guard(guard);
     }
     if let Some(ref return_type_annotation) = *declaration.return_type_annotation {
-        visitor.visit_function_return_type_annotation(return_type_annotation);
+        visitor.visit_fn_ret_ty(return_type_annotation);
     }
-    visitor.visit_function_body(&declaration.body);
+    visitor.visit_fn_body(&declaration.body);
 }
 
-pub fn walk_expression<'ast, V: Visitor<'ast>>(visitor: &mut V, expression: &'ast node::Expr) {
+pub fn walk_expr<'ast, V: Visitor<'ast>>(visitor: &mut V, expression: &'ast node::Expr) {
     match &expression.kind {
         node::ExprKind::None => {}
         node::ExprKind::Block(statements, expression) => {
             visitor.visit_block(statements, expression);
         }
         node::ExprKind::BinaryOp { op: _, lhs, rhs } => {
-            visitor.visit_expression(lhs);
-            visitor.visit_expression(rhs);
+            visitor.visit_expr(lhs);
+            visitor.visit_expr(rhs);
         }
         node::ExprKind::UnaryOp(_, expression) => {
-            visitor.visit_expression(expression);
+            visitor.visit_expr(expression);
         }
         _ => todo!(),
     }
@@ -133,30 +173,30 @@ mod tests {
             self.visited.push("visit_block");
             walk_block(self, statements, expression);
         }
-        fn visit_statement(&mut self, statement: &'ast node::Stmt) {
+        fn visit_stmt(&mut self, statement: &'ast node::Stmt) {
             self.visited.push("visit_statement");
-            walk_statement(self, statement);
+            walk_stmt(self, statement);
         }
-        fn visit_function_declaration(&mut self, declaration: &'ast node::FunctionDeclaration) {
+        fn visit_fn_decl(&mut self, declaration: &'ast node::FunctionDeclaration) {
             self.visited.push("visit_function_declaration");
-            walk_function_declaration(self, declaration);
+            walk_fn_decl(self, declaration);
         }
-        fn visit_expression(&mut self, expression: &'ast node::Expr) {
+        fn visit_expr(&mut self, expression: &'ast node::Expr) {
             self.visited.push("visit_expression");
-            walk_expression(self, expression);
+            walk_expr(self, expression);
         }
-        fn visit_function_parameter(&mut self, _parameter: &'ast node::FunctionParameter) {
+        fn visit_fn_param(&mut self, _parameter: &'ast node::FunctionParameter) {
             self.visited.push("visit_function_parameter");
         }
-        fn visit_function_guard(&mut self, _guard: &'ast node::Expr) {
+        fn visit_fn_guard(&mut self, _guard: &'ast node::Expr) {
             self.visited.push("visit_function_guard");
         }
-        fn visit_function_return_type_annotation(&mut self, _annotation: &'ast node::Ty) {
+        fn visit_fn_ret_ty(&mut self, _annotation: &'ast node::Ty) {
             self.visited.push("visit_function_return_type_annotation");
         }
-        fn visit_function_body(&mut self, body: &'ast node::Expr) {
+        fn visit_fn_body(&mut self, body: &'ast node::Expr) {
             self.visited.push("visit_function_body");
-            walk_function_body(self, body);
+            walk_fn_body(self, body);
         }
     }
 
@@ -197,7 +237,7 @@ mod tests {
             span: Span::default(),
         };
         let mut visitor = TestVisitor { visited: vec![] };
-        walk_statement(&mut visitor, &statement);
+        walk_stmt(&mut visitor, &statement);
         assert_eq!(visitor.visited, vec!["visit_expression"]);
     }
 
@@ -229,7 +269,7 @@ mod tests {
             }),
         };
         let mut visitor = TestVisitor { visited: vec![] };
-        walk_function_declaration(&mut visitor, &declaration);
+        walk_fn_decl(&mut visitor, &declaration);
         assert_eq!(
             visitor.visited,
             vec![
