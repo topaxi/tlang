3ba4aadc25d0c2a4e6f266a3ee1ec9b680487df7
Damian Senn <damian.senn@topaxi.codes>
2024-10-11 13:43:45 +0200

    refactor: codemirror from/to should be readonly

--- crates/tlang_bindings_js/src/codemirror.rs
+++ crates/tlang_bindings_js/src/codemirror.rs
@@ -4,35 +4,30 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct CodemirrorDiagnostic {
-    message: String,
-    severity: String,
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub message: String,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub severity: String,
 
     /// Relative position in the document, our diagnostics are line/column based
+    #[wasm_bindgen(readonly)]
     pub from: u32,
 
     /// Relative position in the document, our diagnostics are line/column based
+    #[wasm_bindgen(readonly)]
     pub to: u32,
 }
 
 #[wasm_bindgen]
 impl CodemirrorDiagnostic {
-    pub fn new(message: &str, severity: &str, from: u32, to: u32) -> Self {
+    pub(crate) fn new(message: &str, severity: &str, from: u32, to: u32) -> Self {
         CodemirrorDiagnostic {
             message: message.to_string(),
             severity: severity.to_string(),
             from,
             to,
         }
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn message(&self) -> String {
-        self.message.clone()
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn severity(&self) -> String {
-        self.severity.clone()
     }
 }
 
