da498cc12e876369cf195eb33c4698fbedbe334f
Damian Senn <damian.senn@topaxi.codes>
2024-10-23 22:43:41 +0200

    feat: hir, define hir ids and lower pattern nodes

--- crates/tlang_ast_lowering/src/lib.rs
+++ crates/tlang_ast_lowering/src/lib.rs
@@ -1,13 +1,33 @@
 #![feature(box_patterns)]
 use tlang_ast as ast;
-use tlang_ast::node::{BinaryOpExpression, FunctionDeclaration, LetDeclaration};
-use tlang_hir::hir;
+use tlang_ast::node::{
+    BinaryOpExpression, EnumPattern, FunctionDeclaration, Ident, IdentifierPattern, LetDeclaration,
+};
+use tlang_ast::token::kw;
+use tlang_hir::hir::{self, HirId};
 
-struct LoweringContext {}
+struct LoweringContext {
+    unique_id: HirId,
+    symbol_id_to_hir_id: std::collections::HashMap<ast::symbols::SymbolId, HirId>,
+}
 
 impl LoweringContext {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            unique_id: HirId::new(0),
+            symbol_id_to_hir_id: std::collections::HashMap::default(),
+        }
+    }
+
+    fn lower_symbol_id(&mut self, id: &ast::symbols::SymbolId) -> HirId {
+        if let Some(hir_id) = self.symbol_id_to_hir_id.get(id) {
+            *hir_id
+        } else {
+            let hir_id = self.unique_id;
+            self.unique_id = self.unique_id.next();
+            self.symbol_id_to_hir_id.insert(*id, hir_id);
+            hir_id
+        }
     }
 
     fn lower_module(&mut self, module: &ast::node::Module) -> hir::Module {
@@ -43,7 +63,7 @@ impl LoweringContext {
                 statements,
                 expression,
                 span,
-                ..
+                symbol_table: _,
             }) => {
                 let kind =
                     hir::ExprKind::Block(Box::new(self.lower_block(statements, expression, *span)));
@@ -113,6 +133,7 @@ impl LoweringContext {
         let return_type = self.lower_ty(&decl.return_type_annotation);
 
         hir::FunctionDeclaration {
+            hir_id: self.lower_symbol_id(&decl.id),
             name,
             parameters,
             return_type,
@@ -121,13 +142,76 @@ impl LoweringContext {
         }
     }
 
+    fn lower_path(&mut self, path: &ast::node::Path) -> hir::Path {
+        let segments = path
+            .segments
+            .iter()
+            .map(|seg| self.lower_path_segment(seg))
+            .collect();
+
+        hir::Path {
+            segments,
+            span: path.span,
+        }
+    }
+
+    fn lower_path_segment(&mut self, seg: &Ident) -> hir::PathSegment {
+        hir::PathSegment { ident: seg.clone() }
+    }
+
     fn lower_pat(&mut self, node: &ast::node::Pattern) -> hir::Pat {
-        match node.kind {
+        match &node.kind {
             ast::node::PatternKind::Wildcard => hir::Pat {
                 kind: hir::PatKind::Wildcard,
                 span: node.span,
             },
-            _ => todo!(),
+            ast::node::PatternKind::Literal(box literal) => hir::Pat {
+                kind: hir::PatKind::Literal(Box::new(literal.clone())),
+                span: node.span,
+            },
+            ast::node::PatternKind::Identifier(box IdentifierPattern { id, name }) => hir::Pat {
+                kind: hir::PatKind::Identifier(self.lower_symbol_id(id), Box::new(name.clone())),
+                span: node.span,
+            },
+            ast::node::PatternKind::List(patterns) => hir::Pat {
+                kind: hir::PatKind::List(patterns.iter().map(|pat| self.lower_pat(pat)).collect()),
+                span: node.span,
+            },
+            ast::node::PatternKind::Rest(pattern) => hir::Pat {
+                kind: hir::PatKind::Rest(Box::new(self.lower_pat(pattern))),
+                span: node.span,
+            },
+            ast::node::PatternKind::Enum(box EnumPattern {
+                // TODO: We lower the identifier to a path, in the AST this is an Expr, but it is
+                //       always a path, we should update the AST to reflect this.
+                identifier,
+                elements,
+                named_fields: _, // In HIR, we no longer care whether it's a named field or not
+            }) => {
+                let path = if let ast::node::ExprKind::Path(path) = &identifier.kind {
+                    path
+                } else {
+                    unreachable!("EnumPattern identifier should be a path, validate AST first")
+                };
+
+                let path = self.lower_path(path);
+                let elements = elements.iter().map(|pat| self.lower_pat(pat)).collect();
+
+                hir::Pat {
+                    kind: hir::PatKind::Enum(Box::new(path), elements),
+                    span: node.span,
+                }
+            }
+            ast::node::PatternKind::_Self(id) => hir::Pat {
+                kind: hir::PatKind::Identifier(
+                    self.lower_symbol_id(id),
+                    Box::new(Ident::new(kw::_Self, node.span)),
+                ),
+                span: node.span,
+            },
+            ast::node::PatternKind::None => {
+                unreachable!("PatternKind::None should not be encountered, validate AST first")
+            }
         }
     }
 
