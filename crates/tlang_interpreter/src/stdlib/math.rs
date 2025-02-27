6bb296b8ff8a0230afd42024ebeba83ca1b00f03
Damian Senn <damian.senn@topaxi.codes>
2025-02-27 15:59:34 +0100

    feat(interpreter): usize/isize to tlang value

--- crates/tlang_interpreter/src/stdlib/math.rs
+++ crates/tlang_interpreter/src/stdlib/math.rs
@@ -5,34 +5,30 @@ use crate::value::{TlangPrimitive, TlangValue};
 
 #[native_fn]
 pub fn floor(_: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
-    match args[0].as_primitive() {
-        TlangPrimitive::Float(f) => TlangValue::F64(f.floor()),
-        TlangPrimitive::Int(i) => TlangValue::I64(i),
-        TlangPrimitive::UInt(i) => TlangValue::U64(i),
+    let value = args[0];
+
+    match value.as_primitive() {
+        TlangPrimitive::Float(f) => TlangValue::from(f.floor()),
+        TlangPrimitive::Int(_) | TlangPrimitive::UInt(_) => value,
         value => panic!("Expected float or int, got {:?}", value),
     }
 }
 
 #[native_fn]
 pub fn sqrt(_: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
-    match args[0].as_primitive() {
-        TlangPrimitive::Float(f) => TlangValue::F64(f.sqrt()),
-        TlangPrimitive::Int(i) => TlangValue::F64(((i) as f64).sqrt()),
-        TlangPrimitive::UInt(i) => TlangValue::F64(((i) as f64).sqrt()),
-        value => panic!("Expected float or int, got {:?}", value),
-    }
+    TlangValue::from(args[0].as_f64().sqrt())
 }
 
 #[native_fn]
 pub fn max(_: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
     match (args[0].as_primitive(), args[1].as_primitive()) {
-        (TlangPrimitive::UInt(i1), TlangPrimitive::UInt(i2)) => TlangValue::U64(i1.max(i2)),
-        (TlangPrimitive::Int(i1), TlangPrimitive::Int(i2)) => TlangValue::I64(i1.max(i2)),
-        (TlangPrimitive::Float(f1), TlangPrimitive::Float(f2)) => TlangValue::F64(f1.max(f2)),
-        (TlangPrimitive::UInt(i), TlangPrimitive::Float(f)) => TlangValue::F64((i as f64).max(f)),
-        (TlangPrimitive::Int(i), TlangPrimitive::Float(f)) => TlangValue::F64((i as f64).max(f)),
-        (TlangPrimitive::Float(f), TlangPrimitive::UInt(i)) => TlangValue::F64(f.max(i as f64)),
-        (TlangPrimitive::Float(f), TlangPrimitive::Int(i)) => TlangValue::F64(f.max(i as f64)),
+        (TlangPrimitive::UInt(i1), TlangPrimitive::UInt(i2)) => TlangValue::from(i1.max(i2)),
+        (TlangPrimitive::Int(i1), TlangPrimitive::Int(i2)) => TlangValue::from(i1.max(i2)),
+        (TlangPrimitive::Float(f1), TlangPrimitive::Float(f2)) => TlangValue::from(f1.max(f2)),
+        (TlangPrimitive::UInt(i), TlangPrimitive::Float(f)) => TlangValue::from((i as f64).max(f)),
+        (TlangPrimitive::Int(i), TlangPrimitive::Float(f)) => TlangValue::from((i as f64).max(f)),
+        (TlangPrimitive::Float(f), TlangPrimitive::UInt(i)) => TlangValue::from(f.max(i as f64)),
+        (TlangPrimitive::Float(f), TlangPrimitive::Int(i)) => TlangValue::from(f.max(i as f64)),
         (value1, value2) => panic!("Expected float or int, got {:?} and {:?}", value1, value2),
     }
 }
@@ -40,15 +36,15 @@ pub fn max(_: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
 #[cfg(feature = "rand")]
 #[native_fn]
 pub fn random(_: &mut InterpreterState, _: &[TlangValue]) -> TlangValue {
-    TlangValue::F64(rand::random())
+    TlangValue::from(rand::random::<f64>())
 }
 
 #[cfg(feature = "rand")]
 #[native_fn]
 pub fn random_int(_: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
     match args[0].as_primitive() {
-        TlangPrimitive::UInt(i) => TlangValue::U64(rand::random::<u64>() % i),
-        TlangPrimitive::Int(i) => TlangValue::I64(rand::random::<i64>() % i),
+        TlangPrimitive::UInt(i) => TlangValue::from(rand::random::<u64>() % i),
+        TlangPrimitive::Int(i) => TlangValue::from(rand::random::<i64>() % i),
         value => panic!("Expected int, got {:?}", value),
     }
 }
