ebd0391034a50814d91c53b8053ec906f686c725
Damian Senn <damian.senn@topaxi.codes>
2025-03-02 19:05:24 +0100

    refactor(interpreter): remove unnecessary casts

--- crates/tlang_interpreter/src/stdlib/collections.rs
+++ crates/tlang_interpreter/src/stdlib/collections.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use log::debug;
 use tlang_macros::native_fn;
 
-use crate::shape::{TlangStructMethod, TlangStructShape};
+use crate::shape::TlangStructMethod;
 use crate::state::InterpreterState;
 use crate::value::{NativeFnReturn, TlangObjectKind, TlangStruct, TlangValue};
 use crate::Interpreter;
@@ -11,8 +11,8 @@ use crate::Interpreter;
 #[native_fn(name = "len")]
 pub fn len(state: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
     match state.get_object(args[0]) {
-        Some(TlangObjectKind::Struct(obj)) => TlangValue::U64(obj.field_values.len() as u64),
-        Some(TlangObjectKind::String(string)) => TlangValue::U64(string.len() as u64),
+        Some(TlangObjectKind::Struct(obj)) => TlangValue::from(obj.field_values.len()),
+        Some(TlangObjectKind::String(string)) => TlangValue::from(string.len()),
         _ => panic!("Expected struct or string, got {:?}", args[0]),
     }
 }
@@ -23,11 +23,6 @@ pub fn define_list_shape(interpreter: &mut Interpreter) {
     list_methods.insert(
         "slice".to_string(),
         TlangStructMethod::from_value(interpreter.create_native_fn(|state, args| {
-            debug!(
-                "Calling slice on list, args: {:?}",
-                args.iter().map(|a| state.stringify(*a)).collect::<Vec<_>>()
-            );
-
             let this = state
                 .get_object(args[0])
                 .and_then(|o| o.get_struct())
