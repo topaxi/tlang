8d1ede4d1ae34247272c83fc0642fd3ebda96856
Damian Senn <damian.senn@topaxi.codes>
2025-03-10 09:26:01 +0100

    refactor(interpreter): move macros into their own module

--- crates/tlang_interpreter/src/lib.rs
+++ crates/tlang_interpreter/src/lib.rs
@@ -17,6 +17,7 @@ use self::value::{
     TlangStruct, TlangValue,
 };
 
+mod macros;
 mod resolver;
 mod scope;
 mod shape;
@@ -33,48 +34,6 @@ pub struct NativeFn {
 }
 
 inventory::collect!(NativeFn);
-
-/// Propagate control flow (`Return`, `TailCall`, etc.), otherwise extract the value.
-macro_rules! eval_value {
-    ($expr:expr) => {
-        match $expr {
-            EvalResult::Value(val) => val,
-            other => return other,
-        }
-    };
-}
-
-/// Propagate control flow (`Return`, `TailCall`, etc.), otherwise extract the value.
-macro_rules! eval_match_value {
-    ($expr:expr) => {
-        match $expr {
-            EvalResult::Value(val) => val,
-            other => return MatchResult::NotMatched(other),
-        }
-    };
-}
-
-/// Propagate control flow if it's not `Value`
-macro_rules! propagate {
-    ($expr:expr) => {
-        match $expr {
-            EvalResult::Value(_) | EvalResult::Void => {}
-            other => return other,
-        }
-    };
-}
-
-/// Evaluate a list of hir:Expr expressions into a vector of values, propagating control flow if necessary.
-macro_rules! eval_exprs {
-    ($this:expr, $eval:expr, $exprs:expr) => {{ eval_exprs!($this, $eval, $exprs, $exprs.len()) }};
-    ($this:expr, $eval:expr, $exprs:expr, $capacity:expr) => {{
-        let mut exprs = Vec::with_capacity($capacity);
-        for expr in &$exprs {
-            exprs.push(eval_value!($eval($this, expr)));
-        }
-        exprs
-    }};
-}
 
 #[derive(Debug, Clone, Copy)]
 pub enum EvalResult {
