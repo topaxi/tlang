46411e91feb25a916e55d2cf7e5ed76795f2c68c
Damian Senn <damian.senn@topaxi.codes>
2025-04-04 14:00:22 +0200

    wip(hir_opt): constant folding

--- crates/tlang_hir_opt/tests/constant_folding.rs
+++ crates/tlang_hir_opt/tests/constant_folding.rs
@@ -1,91 +1,49 @@
 use insta::assert_snapshot;
 use tlang_hir_pretty::HirPretty;
-use tlang_hir_opt::constant_folding::ConstantFolder;
-use tlang_hir_opt::hir_opt::HirOptimizer;
 
 mod common;
 
 #[test]
 fn simple_binary_constant_folding() {
-    let source = r#"
-        let x = 1 + 2;
-        println?(x);
-    "#;
+    let hir = common::compile_and_optimize(
+        r#"
+        fn main() {
+            let x = 1 + 2;
+            println(x);
+        }
+        "#,
+    );
 
-    let mut hir = tlang_hir::hir_from_source(source).unwrap();
-    let mut optimizer = HirOptimizer::new();
-    optimizer.add_pass(Box::new(ConstantFolder::new()));
-    optimizer.optimize_module(&mut hir);
+    let mut prettier = HirPretty::default();
+    prettier.print_module(&hir);
 
-    let formatted = tlang_hir::hir_to_string(&hir);
-    insta::assert_snapshot!(formatted, @"println?(3);");
-}
-
-#[test]
-fn multiplication_constant_folding() {
-    let source = r#"
-        let x = 2 * 3;
-        println?(x);
-    "#;
-
-    let mut hir = tlang_hir::hir_from_source(source).unwrap();
-    let mut optimizer = HirOptimizer::new();
-    optimizer.add_pass(Box::new(ConstantFolder::new()));
-    optimizer.optimize_module(&mut hir);
-
-    let formatted = tlang_hir::hir_to_string(&hir);
-    insta::assert_snapshot!(formatted, @"println?(6);");
-}
-
-#[test]
-fn chained_operations() {
-    let source = r#"
-        let x = 1 + 2;
-        let y = x * 3;
-        let z = y - 4;
-        println?(z);
-    "#;
-
-    let mut hir = tlang_hir::hir_from_source(source).unwrap();
-    let mut optimizer = HirOptimizer::new();
-    optimizer.add_pass(Box::new(ConstantFolder::new()));
-    optimizer.optimize_module(&mut hir);
-
-    let formatted = tlang_hir::hir_to_string(&hir);
-    insta::assert_snapshot!(formatted, @"println?(5);");
-}
-
-#[test]
-fn mixed_operations() {
-    let source = r#"
-        let x = (1 + 2) * (3 - 1);
-        println?(x);
-    "#;
-
-    let mut hir = tlang_hir::hir_from_source(source).unwrap();
-    let mut optimizer = HirOptimizer::new();
-    optimizer.add_pass(Box::new(ConstantFolder::new()));
-    optimizer.optimize_module(&mut hir);
-
-    let formatted = tlang_hir::hir_to_string(&hir);
-    insta::assert_snapshot!(formatted, @"println?(6);");
+    assert_snapshot!(prettier.output(), @r###"
+    fn main() -> unknown {
+        let x: unknown = 3;
+        println?(3);
+    };
+    "###);
 }
 
 #[test]
 #[ignore]
 fn chained_constant_folding() {
-    let source = r#"
-        let x = 1 + 2;
-        let y = x * 3;
-        let z = y + 4;
-        println?(z);
-    "#;
+    let hir = common::compile_and_optimize(
+        r#"
+        fn main() {
+            let x = 1 + 2;
+            let y = x * 3;
+            println(y);
+        }
+        "#,
+    );
 
-    let mut hir = tlang_hir::hir_from_source(source).unwrap();
-    let mut optimizer = HirOptimizer::new();
-    optimizer.add_pass(Box::new(ConstantFolder::new()));
-    optimizer.optimize_module(&mut hir);
+    let mut prettier = HirPretty::default();
+    prettier.print_module(&hir);
 
-    let formatted = tlang_hir::hir_to_string(&hir);
-    insta::assert_snapshot!(formatted, @"println?(13);");
+    assert_snapshot!(prettier.output(), @r###"
+    fn main() -> unknown {
+        println?(9);
+    };
+    "###);
 }
