6ead8756589dea6fa1477926bc5f1ecfbc9043d4
Damian Senn <damian.senn@topaxi.codes>
2025-08-07 22:17:33 +0200

    fix: accidentally committed changes

--- crates/tlang_hir_opt/src/lib.rs
+++ crates/tlang_hir_opt/src/lib.rs
@@ -2,9 +2,7 @@
 pub mod constant_folding;
 pub mod constant_propagation;
 pub mod hir_opt;
-pub mod symbol_resolution;
 
 pub use constant_folding::ConstantFolder;
 pub use constant_propagation::ConstantPropagator;
 pub use hir_opt::{HirOptimizer, HirPass};
-pub use symbol_resolution::SymbolResolution;
