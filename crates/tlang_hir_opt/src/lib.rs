9556473118ea622abe9b11314737f670f493e802
Damian Senn <damian.senn@topaxi.codes>
2025-08-08 19:51:36 +0200

    wip: resolve step in hir

--- crates/tlang_hir_opt/src/lib.rs
+++ crates/tlang_hir_opt/src/lib.rs
@@ -2,7 +2,9 @@
 pub mod constant_folding;
 pub mod constant_propagation;
 pub mod hir_opt;
+pub mod symbol_resolution;
 
 pub use constant_folding::ConstantFolder;
 pub use constant_propagation::ConstantPropagator;
 pub use hir_opt::{HirOptimizer, HirPass};
+pub use symbol_resolution::SymbolResolution;
