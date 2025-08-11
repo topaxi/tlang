a09d1a504573b2c9263fd74e4bfb316547db1976
Damian Senn <damian.senn@topaxi.codes>
2025-08-11 21:39:30 +0200

    refactor: minor dedupe

--- crates/tlang_runtime/tlang_stdlib/src/globals.rs
+++ crates/tlang_runtime/tlang_stdlib/src/globals.rs
@@ -1,25 +1,22 @@
 use tlang_macros::native_fn;
 use tlang_memory::{InterpreterState, TlangValue};
 
+fn stringify(state: &mut InterpreterState, values: &[TlangValue]) -> String {
+    values
+        .iter()
+        .map(|v| state.stringify(*v))
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
 #[native_fn]
 pub fn log(state: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
-    println!(
-        "{}",
-        args.iter()
-            .map(|v| state.stringify(*v))
-            .collect::<Vec<_>>()
-            .join(" ")
-    );
+    println!("{}", stringify(state, args));
     TlangValue::Nil
 }
 
 #[native_fn]
 pub fn panic(state: &mut InterpreterState, args: &[TlangValue]) -> TlangValue {
-    state.panic(format!(
-        "Panic!: {}",
-        args.iter()
-            .map(|v| state.stringify(*v))
-            .collect::<Vec<_>>()
-            .join(" ")
-    ));
+    let stringified = stringify(state, args);
+    state.panic(format!("Panic!: {}", stringified));
 }
